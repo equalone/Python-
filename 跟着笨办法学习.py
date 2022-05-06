ccea32e3a5b34ed0315e6bf845c520327dadb25b
cole yi <92663171+equalone@users.noreply.github.com>
2022-05-07 05:43:49 +0800

    Create 跟着笨办法学习.py

---
+++ 跟着笨办法学习.py
@@ -0,0 +1,54 @@
+# python e:\gedit\save\exp4.py
+
+#we have cars 100
+
+cars=100
+
+#a car can hold four people
+
+space_in_a_car=4.0
+
+#we had 30 drivers
+
+drivers=30
+
+#wi have passengers 90 
+
+passenger=90
+
+#empty car equals the number of cars minus the number of drivers
+
+cars_not_driven=cars-drivers
+
+cars_driven=drivers
+
+carpool_capacity=cars_driven*space_in_a_car
+
+average_passengers_per_car=passenger/cars_driven
+
+print("there are",cars,"cars available.")
+
+print("there are only",drivers,"drivers available.")
+
+print("there will be",cars_not_driven,"empty cars today.")
+
+print("we have",passenger,"to carpool today.")
+
+print("we need to put about",average_passengers_per_car,"in each car")
+
+i=95874;j=2577;x=7;y=8
+
+print("7+8=",x+y)
+
+print("95874-2577=",i-j)
+
+print("7*8=",x*y)
+
+print("2577/7=",j/x)
+
+print("95874%8=",i%y)
+
+print("7//8=",x//y)
+
+
+
